988cb240e43d1e605168efed139d4d5029ab6438
kk++ <kkplusplus@163.com>
2024-06-14 15:01:58 +0800

    build: 为部署github page修改配置

--- apps/web-sample/vite.config.ts
+++ apps/web-sample/vite.config.ts
@@ -7,12 +7,6 @@ export default defineConfig({
   server: {
     port: 1100,
   },
-  build: {
-    rollupOptions: {
-      output: { globals: { vue: 'Vue', 'tdesign-vue-next': 'tdesign-vue-next' } },
-      external: ['vue', 'tdesign-vue-next'],
-    },
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
