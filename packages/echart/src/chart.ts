9f41a254989b3007b528a53ddb622797cec4f8dd
kk++ <kkplusplus@163.com>
2024-06-23 22:29:06 +0800

    feat: 新增echart包

---
+++ packages/echart/src/chart.ts
@@ -0,0 +1,25 @@
+import { onMounted, ref, onBeforeUnmount } from 'vue'
+import * as echarts from 'echarts'
+import { debounce } from './utils'
+
+export function useChart(key: string, options: any) {
+  let chart: echarts.EChartsType | null = null
+
+  const resizeHandler = debounce(() => {
+    if (chart) {
+      chart.resize()
+    }
+  }, 100)
+
+  onMounted(() => {
+    chart = echarts.init(document.getElementById(key))
+    chart.setOption(options || {})
+    window.addEventListener('resize', resizeHandler)
+  })
+
+  onBeforeUnmount(() => {
+    window.removeEventListener('resize', resizeHandler)
+  })
+
+  return { chart }
+}
---
+++ packages/echart/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from "path"
+
+export default defineConfig({
+  server: {
+    port: 1102,
+  },
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: './src/index.ts',
+      name: 'vue-one-piece',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      output: { globals: { vue: 'Vue', 'tdesign-vue-next': 'tdesign-vue-next' } },
+      external: ['vue', 'tdesign-vue-next'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [vue()],
+})
