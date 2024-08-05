9c5b00bebd2ad03e0ef1e7400d00204808b0a0fc
kk++ <kkplusplus@163.com>
2024-08-05 14:53:58 +0800

    refactor: 为更好的类型提示,重构useTable

--- packages/element-plus/src/table.ts
+++ packages/element-plus/src/table.ts
@@ -1,8 +1,15 @@
-import { PageInfo } from "tdesign-vue-next";
-import { TableOptions, useTable as useVueTable } from "@2kk/admin-vueuse";
+import { PartialSearchQuery, ResponseProps, useTable as useVueTable } from "@2kk/admin-vueuse";
 
-export function useTable(options: TableOptions) {
-  const { getList, list, listLoading, total, pagination, searchQuery, handleCurrentPageChange, handlePageSizeChange } = useVueTable(options);
+export function useTable<TSearchQuery extends PartialSearchQuery, TTarget = any>(
+  tableDataResolver: (...args: any[]) => Promise<any>,
+  searchQuery: TSearchQuery | undefined = { pageNum: 1, pageSize: 20 } as TSearchQuery,
+  isPagination: boolean | undefined = true,
+  hasMounted: boolean | undefined = true,
+  shim: <TSource>(list: TSource[]) => TTarget[] = (list: any[]) => list,
+  responseProps: ResponseProps | undefined = { resultData: 'list', total: 'count' }
+) {
+  const { getList, list, listLoading, pagination, searchQuery: searchQueryInner, handleCurrentPageChange, handlePageSizeChange }
+    = useVueTable(tableDataResolver, searchQuery, isPagination, hasMounted, shim, responseProps);
 
   const handlePageChange = (currentPage: number, pageSize: number) => {
     searchQuery.value.pageSize = pageSize
@@ -12,12 +19,11 @@ export function useTable(options: TableOptions) {
 
   return {
     list,
-    total,
     listLoading,
     getList,
     handleCurrentPageChange,
     handlePageSizeChange,
-    searchQuery,
+    searchQuery: searchQueryInner,
     pagination,
     handlePageChange
   }
