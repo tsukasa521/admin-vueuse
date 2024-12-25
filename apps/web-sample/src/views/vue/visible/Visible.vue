c1068f42f1957a65e051d4865df6566f222d5787
kk++ <kkplusplus@163.com>
2024-12-25 22:18:56 +0800

    test: vue包下增加useVisible示例

--- apps/web-sample/src/views/vue/visible/Visible.vue
+++ apps/web-sample/src/views/vue/visible/Visible.vue
@@ -13,17 +13,45 @@
       <p class="bg-base py-2 px-5 cursor-pointer w-32" @click="open">点击打开</p>
       <br />
       <br />
-      <Dialog v-model:visible="visible" />
+      <Dialog v-model:visible="visible" @resolve="resolve" />
+    </SamplePanel>
+
+    <SamplePanel title="新增/修改场景" description="当子元素中包含时，全部子元素会水平排列，否则会垂直排列。">
+      <p class="bg-base py-2 px-5 cursor-pointer w-32" @click="openAddUpdateDialogWrapper">点击打开</p>
+      <br />
+      <br />
+      <AddUpdateDialog v-model:visible="addUpdateDialogVisible" :dataSource="addUpdateDataSource" @resolve="resolve" />
     </SamplePanel>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useExternalVisible } from "@2kk/admin-vueuse";
-import { Dialog } from "./components";
+import { Dialog, AddUpdateDialog } from "./components";
+
+type User = {
+  id: string,
+  name: string,
+  age: number
+}
 
 const { visible, open, close } = useExternalVisible();
 
+const { visible: addUpdateDialogVisible, open: openAddUpdateDialog, dataSource: addUpdateDataSource } = useExternalVisible<User>();
+
+const openAddUpdateDialogWrapper = () => {
+  addUpdateDataSource.value = {
+    id: '1',
+    name: 'kk',
+    age: 18
+  }
+  openAddUpdateDialog()
+}
+
+const resolve = () => {
+  alert('点击了确定')
+  close()
+}
 </script>
 
 <style lang="scss" scoped></style>
--- apps/web-sample/src/views/vue/visible/components/AddUpdateDialog.vue
+++ apps/web-sample/src/views/vue/visible/components/AddUpdateDialog.vue
@@ -1,6 +1,8 @@
 <template>
   <div v-if="currentVisible" class="border border-solid rounded-xl p-5">
-    <div>这是一段内容</div>
+    <div>{{ title }}</div>
+    <div>姓名：{{ props.dataSource.name }}</div>
+    <div>年龄：{{ props.dataSource.age }}</div>
     <div>
       <p class="bg-base py-2 px-5 cursor-pointer" @click="closed">关闭</p>
       <p class="bg-base py-2 px-5 cursor-pointer" @click="resolve">确定</p>
@@ -9,15 +11,17 @@
 </template>
 
 <script setup lang="ts">
-import { useVisible, VisibleEmits, visibleProps } from '@2kk/admin-vueuse';
+import { dataSourceVisibleProps, useAddUpdateVisible, useVisible, VisibleEmits, visibleProps, } from '@2kk/admin-vueuse';
 
 const props = defineProps({
-  ...visibleProps
+  ...visibleProps,
+  ...dataSourceVisibleProps
 });
 
 const emits = defineEmits<VisibleEmits>()
 
 const { currentVisible, closed } = useVisible(props, emits)
+const { title, isAdd, isUpdate } = useAddUpdateVisible(props, emits, { title: '用户' })
 
 const resolve = () => {
   emits('resolve')
